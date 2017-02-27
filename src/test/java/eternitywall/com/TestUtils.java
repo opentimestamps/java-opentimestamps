a03c93957935fdb771c4fc67dc77cd76ccf153a1
Riccardo Casatta <riccardo@casatta.it>
2017-02-27 13:17:10 +0100

    adding first test

---
+++ src/test/java/eternitywall/com/TestUtils.java
@@ -0,0 +1,17 @@
+package eternitywall.com;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by casatta on 27/02/17.
+ */
+public class TestUtils {
+
+    @Test
+    public void testBytesToHex() {
+        assertTrue(true);
+
+    }
+}
