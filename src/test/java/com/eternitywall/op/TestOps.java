f376c133a51b6b468b3afb0edb8948740ac3c470
Riccardo Casatta <riccardo@casatta.it>
2017-03-06 15:36:39 +0100

    adding test of other ops

--- src/test/java/com/eternitywall/op/TestOps.java
+++ src/test/java/com/eternitywall/op/TestOps.java
@@ -17,7 +17,40 @@ public class TestOps {
 
         byte[] result2 = new OpKECCAK256().call(Utils.hexToBytes("80"));
         assertEquals("56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421", Utils.bytesToHex(result2));
-
-
     }
+
+    @Test
+    public void testSha1() {
+        byte[] result = new OpSHA1().call(Utils.hexToBytes("0a"));
+        assertEquals("adc83b19e793491b1c6ea0fd8b46cd9f32e592fc", Utils.bytesToHex(result));
+    }
+
+    @Test
+    public void testSha256() {
+        byte[] result = new OpSHA256().call(Utils.hexToBytes(""));
+        assertEquals("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", Utils.bytesToHex(result));
+    }
+
+    @Test
+    public void testRipemd() {
+        byte[] result = new OpRIPEMD160().call(Utils.hexToBytes(""));
+        assertEquals("9c1185a5c5e9fc54612808977ee8f548b2258d31", Utils.bytesToHex(result));
+    }
+
+    @Test
+    public void testAppend() {
+        byte[] foos = Utils.hexToBytes("00");
+        byte[] bars = Utils.hexToBytes("11");
+        byte[] call = new OpAppend(foos).call(bars);
+        assertEquals("1100", Utils.bytesToHex(call));
+    }
+
+    @Test
+    public void testPrepend() {
+        byte[] foos = Utils.hexToBytes("00");
+        byte[] bars = Utils.hexToBytes("11");
+        byte[] call = new OpPrepend(foos).call(bars);
+        assertEquals("0011", Utils.bytesToHex(call));
+    }
+
 }
